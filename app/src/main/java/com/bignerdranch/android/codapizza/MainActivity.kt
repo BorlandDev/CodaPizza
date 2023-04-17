160036a975e22d7724cc9631dbacce7a321ec515
BorlandDev <minsk1115596@gmail.com>
2023-04-17 17:55:59 +0300

    added PizzaBuilderScreen

--- app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.bignerdranch.android.codapizza.model.Topping
+import com.bignerdranch.android.codapizza.model.ToppingPlacement
+import com.bignerdranch.android.codapizza.ui.PizzaBuilderScreen
 import com.bignerdranch.android.codapizza.ui.ToppingCell
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToppingCell()
+            PizzaBuilderScreen()
         }
     }
 }
